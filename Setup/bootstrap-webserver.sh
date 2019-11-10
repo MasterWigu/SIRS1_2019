478fb699ede9e98281839aa4e7d8aaaa0f1eb065
Miguel Oliveira <moreira.oliveira@live.com.pt>
2019-11-10 18:21:26 +0000

    asdasd

--- Setup/bootstrap-webserver.sh
+++ Setup/bootstrap-webserver.sh
@@ -14,4 +14,22 @@ apt-get -y install mysql-server
 
 mysql -u root -ptoor < /home/vagrant/website/setup.sql
 
+#create certificates
+cd /home/vagrant
+mkdir httpsCert
+cd httpsCert
+
+openssl genrsa -out privatekey.pem 2048 -noout
+openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 365 -subj "/C=PT/ST=Lisbon/L=Oeiras/O=IST_SIRS2019/OU=Proj/CN=scoreboardWeb/emailAddress=mail@mail.mail"
+openssl x509 -x509toreq -days 365 -in publickey.cer -signkey privatekey.pem -out ca.req
+openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
+
+cd ..
+mkdir customCert
+cd customCert
+openssl genrsa -out privatekey.pem 2048 -noout
+openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 365 -subj "/C=PT/ST=Lisbon/L=Oeiras/O=IST_SIRS2019/OU=Proj/CN=scoreboardCustom/emailAddress=mail@mail.mail"
+openssl x509 -x509toreq -days 365 -in publickey.cer -signkey privatekey.pem -out ca.req
+openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
+
 service nginx restart
