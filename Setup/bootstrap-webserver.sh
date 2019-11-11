b9ccb4e28a2d61ba963ab6210c254ac8698b7e00
InesAlbano <necas98@gmail.com>
2019-11-11 15:51:55 +0000

    Revert "Merge branch 'master' of https://github.com/MasterWigu/SIRS1_2019"
    This reverts commit bc6e34cc210e856781b2cd8769df08acfe543912, reversing
    changes made to 463a37cf5baf888f5b5faca267e17deb6a9d032c.

--- Setup/bootstrap-webserver.sh
+++ Setup/bootstrap-webserver.sh
@@ -5,7 +5,7 @@ apt-get -y install nginx
 
 cp -f /home/vagrant/website/nginx.conf /etc/nginx/nginx.conf
 cp -f /home/vagrant/website/default-site /etc/nginx/sites-available/default
-cp -rf /home/vagrant/website/html /usr/share/nginx/
+cp -f /home/vagrant/website/index.html /usr/share/nginx/html/index.html
 
 echo 'mysql-server mysql-server/root_password password toor' | debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password toor' | debconf-set-selections
@@ -13,15 +13,6 @@ echo 'mysql-server mysql-server/root_password_again password toor' | debconf-set
 apt-get -y install mysql-server
 
 mysql -u root -ptoor < /home/vagrant/website/setup.sql
-
-
-apt-get -y install software-properties-common
-add-apt-repository ppa:ondrej/php
-apt-get update
-apt-get -y install php7.3
-apt-get -y install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml
-update-alternatives --set php /usr/bin/php7.3
-
 
 #create certificates
 cd /home/vagrant
