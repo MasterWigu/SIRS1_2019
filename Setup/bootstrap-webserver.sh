bae0abce15bbde1e82c6dfbee783f4f1d44b299d
Miguel Oliveira <moreira.oliveira@live.com.pt>
2019-11-12 14:40:25 +0000

    adsnadsj2

--- Setup/bootstrap-webserver.sh
+++ Setup/bootstrap-webserver.sh
@@ -25,11 +25,11 @@ apt-get update
 apt install php7.3-fpm php7.3-common php7.3-mysql php7.3-xml php7.3-xmlrpc php7.3-curl php7.3-gd php7.3-imagick php7.3-cli php7.3-dev php7.3-imap php7.3-mbstring php7.3-opcache php7.3-soap php7.3-zip unzip -y
 update-alternatives --set php /usr/bin/php7.3
 
-
+nginx -s reload
 systemctl reload nginx
 
 #create certificates
-cd /home/vagrant
+cd /home/vagrant/
 mkdir httpsCert
 cd httpsCert
 
@@ -37,8 +37,10 @@ openssl genrsa -out privatekey.pem
 openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 365 -subj "/C=PT/ST=Lisbon/L=Oeiras/O=IST_SIRS2019/OU=Proj/CN=scoreboardWeb/emailAddress=mail@mail.mail"
 openssl x509 -x509toreq -days 365 -in publickey.cer -signkey privatekey.pem -out ca.req
 openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
+chmod 400 privatekey.pem
 
-cd ..
+
+cd /home/vagrant/
 mkdir customCert
 cd customCert
 openssl genrsa -out privatekey.pem
