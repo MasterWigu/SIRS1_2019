ae0c33aba224a9920ae3ae2f30e5fe58c4f26d53
Miguel Oliveira <moreira.oliveira@live.com.pt>
2019-11-12 13:05:01 +0000

    asdljnsd

--- Setup/bootstrap-webserver.sh
+++ Setup/bootstrap-webserver.sh
@@ -33,7 +33,7 @@ cd /home/vagrant
 mkdir httpsCert
 cd httpsCert
 
-openssl genrsa -out privatekey.pem 2048 -noout
+openssl genrsa -out privatekey.pem
 openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 365 -subj "/C=PT/ST=Lisbon/L=Oeiras/O=IST_SIRS2019/OU=Proj/CN=scoreboardWeb/emailAddress=mail@mail.mail"
 openssl x509 -x509toreq -days 365 -in publickey.cer -signkey privatekey.pem -out ca.req
 openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
@@ -41,6 +41,7 @@ openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
 cd ..
 mkdir customCert
 cd customCert
-openssl genrsa -out privatekey.pem 2048 -noout
+openssl genrsa -out privatekey.pem
 openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 365 -subj "/C=PT/ST=Lisbon/L=Oeiras/O=IST_SIRS2019/OU=Proj/CN=scoreboardCustom/emailAddress=mail@mail.mail"
 openssl x509 -x509toreq -days 365 -in publickey.cer -signkey privatekey.pem -out ca.req
+openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
