c461f56b2efbc354f1f62c1b8761edc9f754b56c
Miguel Oliveira <moreira.oliveira@live.com.pt>
2019-11-12 12:19:56 +0000

    yesy

--- Setup/bootstrap-client.sh
+++ Setup/bootstrap-client.sh
@@ -4,5 +4,3 @@
 apt-get -y install dictionaries-common
 apt-get -y install xfce4
 apt-get -y install firefox
-
-service lightdm start
--- Setup/bootstrap-webserver.sh
+++ Setup/bootstrap-webserver.sh
@@ -4,8 +4,12 @@
 apt-get -y install nginx
 
 cp -f /home/vagrant/website/nginx.conf /etc/nginx/nginx.conf
-cp -f /home/vagrant/website/default-site /etc/nginx/sites-available/default
-cp -rf /home/vagrant/website/html /usr/share/nginx/
+cp -f /home/vagrant/website/default-site /etc/nginx/sites-available/ScoreboardServer
+rm -rf /var/www/html
+cp -rf /home/vagrant/website/html /var/www/
+
+ln -sf /etc/nginx/sites-available/ScoreboardServer /etc/nginx/sites-enabled/
+unlink /etc/nginx/sites-enabled/default
 
 echo 'mysql-server mysql-server/root_password password toor' | debconf-set-selections
 echo 'mysql-server mysql-server/root_password_again password toor' | debconf-set-selections
@@ -18,10 +22,11 @@ mysql -u root -ptoor < /home/vagrant/website/setup.sql
 apt-get -y install software-properties-common
 add-apt-repository ppa:ondrej/php
 apt-get update
-apt-get -y install php7.3
-apt-get -y install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml
+apt install php7.3-fpm php7.3-common php7.3-mysql php7.3-xml php7.3-xmlrpc php7.3-curl php7.3-gd php7.3-imagick php7.3-cli php7.3-dev php7.3-imap php7.3-mbstring php7.3-opcache php7.3-soap php7.3-zip unzip -y
 update-alternatives --set php /usr/bin/php7.3
 
+
+systemctl reload nginx
 
 #create certificates
 cd /home/vagrant
@@ -40,5 +45,3 @@ openssl genrsa -out privatekey.pem 2048 -noout
 openssl req -new -x509 -key privatekey.pem -out publickey.cer -days 365 -subj "/C=PT/ST=Lisbon/L=Oeiras/O=IST_SIRS2019/OU=Proj/CN=scoreboardCustom/emailAddress=mail@mail.mail"
 openssl x509 -x509toreq -days 365 -in publickey.cer -signkey privatekey.pem -out ca.req
 openssl x509 -req -days 365 -in ca.req -signkey privatekey.pem -out server.crt
-
-service nginx restart
