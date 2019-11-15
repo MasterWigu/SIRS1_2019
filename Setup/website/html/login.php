517be8ca5ebd40b295ebe2cf8f72f69791266802
InesAlbano <necas98@gmail.com>
2019-11-15 00:07:33 +0000

    logout + table of users homepage

--- Setup/website/html/login.php
+++ Setup/website/html/login.php
@@ -1,45 +1,86 @@
-<?php
-    session_start();
-    $DB_HOST = 'localhost';
-    $DB_USER = 'root';
-    $DB_PASS = 'toor';
-    $DB_NAME = 'AVD_SCORE';
-    $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-    if ( !$connection ) {
-        die ('Connection failed: ' . mysqli_connect_error());
-    }
-
-    if ($stmt = $connection->prepare('SELECT password FROM user WHERE username = ?')) {
-      if (!$stmt->bind_param("s", $username)) {
-           echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-      }
-      $username = $_POST['username'];
-      $password = $_POST['password'];
-
-      if (!$stmt->execute()) {
-          echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-      }
-
-      echo "AAAA" . $stmt->num_rows . "BBB";
-      $result = $stmt->get_result();
-      if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-          if (!password_verify($password, $row['password'])) {
-            echo 'Wrong password, try again.';
-          } else {
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['username'] = $username;
-            echo 'Welcome ' . $_SESSION['name'] . '!';
-          }
-          break;
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+        session_start();
+        $DB_HOST = 'localhost';
+        $DB_USER = 'root';
+        $DB_PASS = 'toor';
+        $DB_NAME = 'AVD_SCORE';
+        $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+        if ( !$connection ) {
+            die ('Connection failed: ' . mysqli_connect_error());
         }
 
-      } else {
-        echo 'Invalid username, try again.';
-      }
-    } else {
-        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-    }
+        if ($stmt = $connection->prepare('SELECT password FROM user WHERE username = ?')) {
+          if (!$stmt->bind_param("s", $username)) {
+               echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+          }
+          $username = $_POST['username'];
+          $password = $_POST['password'];
 
-    $connection->close();
-?>
+          if (!$stmt->execute()) {
+              echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+          }
+
+          $result = $stmt->get_result();
+          if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+              if (!password_verify($password, $row['password'])) {
+                echo 'Wrong password, try again.';
+                echo '<br><br><input type="button" value="Back" onclick="history.back(-1)"/>';
+              } else {
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['username'] = $username;
+
+                echo '<div id="header" style="overflow: hidden; padding: 20px 10px; background-color: #f1f1f1;">';
+                echo '<div id="header-left" style="background-color: #f1f1f1; float: left; color: #23a9cf;">';
+                echo '<h1 style="text-align:center;"> Welcome ' . $_SESSION['username'] . '! </h1>';
+                echo '</div>';
+
+                echo '<div id="header-right" style="background-color: #f1f1f1; float: right; text-align:center">';
+                echo '<a href="logout.php" style="padding: 20px 0px; text-decoration:none; color:#7d1e1e; margin-top:auto; margin-bottom:auto">Logout</a>';
+                echo '</div>';
+                echo '</div>';
+
+                echo '<h2 style="text-align:center;color: olivedrab"> Team users </h2>';
+
+                if ($stmt = $connection->prepare('SELECT * FROM user')) {
+                  if (!$stmt->execute()) {
+                      echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                  }
+                  $result = $stmt->get_result();
+                  echo '<table border="5" style="width:70%; margin-left:auto;margin-right:auto;">';
+                  echo '<tr>';
+                  echo '<th> Username </th>';
+                  echo '<th> Points </th>';
+                  echo '</tr>';
+                  while($row = $result->fetch_assoc()){
+                      $name   = $row['username'];
+                      $points = $row['points'];
+                      echo '<tr>';
+                      echo '<th>' . $name . '</th>';
+                      echo '<th>' . $points . '</th>';
+                      echo '</tr>';
+                  }
+                }
+
+              }
+              break;
+            }
+
+          } else {
+            echo 'Invalid username, try again.';
+            echo '<br><br><input type="button" value="Back" onclick="history.back(-1)"/>';
+          }
+        } else {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+
+        $connection->close();
+     ?>
+
+    </body>
+</html>
--- Setup/website/html/register.php
+++ Setup/website/html/register.php
@@ -1,27 +1,37 @@
-<?php
-    $DB_HOST = 'localhost';
-    $DB_USER = 'root';
-    $DB_PASS = 'toor';
-    $DB_NAME = 'AVD_SCORE';
-    $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-    if ( !$connection ) {
-        die ('Connection failed: ' . mysqli_connect_error());
-    }
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+        $DB_HOST = 'localhost';
+        $DB_USER = 'root';
+        $DB_PASS = 'toor';
+        $DB_NAME = 'AVD_SCORE';
+        $connection = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+        if ( !$connection ) {
+            die ('Connection failed: ' . mysqli_connect_error());
+        }
 
 
-    if ($stmt = $connection->prepare('INSERT INTO user (username, password)  VALUES (?, ?)')) {
-      if (!$stmt->bind_param("ss", $username, $password)) {
-        echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-      }
-      $username = $_POST['username'];
-      $password = password_hash($_POST['password'] , PASSWORD_DEFAULT);
+        if ($stmt = $connection->prepare('INSERT INTO user (username, password)  VALUES (?, ?)')) {
+          if (!$stmt->bind_param("ss", $username, $password)) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+          }
+          $username = $_POST['username'];
+          $password = password_hash($_POST['password'] , PASSWORD_DEFAULT);
 
-      if (!$stmt->execute()) {
-        echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-      }
-    } else {
-        echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-    }
-    echo 'User added.';
-    $connection->close();
-?>
+          if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+          }
+        } else {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+        echo 'User added.';
+        $connection->close();
+    ?>
+    <br>
+    <br>
+    <input type="button" value="Back" onclick="history.back(-1)" />
+  </body>
+<html>
