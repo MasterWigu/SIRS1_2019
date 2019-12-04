4cc321edea5051493f44621b6fc666bce9ca249a
InesAlbano <necas98@gmail.com>
2019-12-04 11:26:21 +0000

    client sh

--- Setup/bootstrap-client.sh
+++ Setup/bootstrap-client.sh
@@ -3,3 +3,10 @@
 apt-get -y install dictionaries-common
 apt-get -y install xfce4
 apt-get -y install firefox
+apt-get -y install python3
+apt-get -y install python3-pip
+apt-get -y install python3-dev libffi-dev build-essential virtualenvwrapper
+pip3 install virtualenvwrapper
+source "/usr/bin/virtualenvwrapper.sh"
+export WORKON_HOME="/opt/virtual_env/"
+mkvirtualenv --python=$(which python3) angr && python3 -m pip install angr
